5a8844d16f8e90df100463504b0cee1b2d2a9ac5
James Jessen <JSJessen@gmail.com>
2020-10-23 22:11:10 -0700

    Incremented cache version

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -8,7 +8,7 @@ const filesToCache = [
   'pages/404.html'
 ];
 
-const staticCacheName = 'cache-v1.0';
+const staticCacheName = 'cache-v1.1';
 console.log(staticCacheName);
 
 self.addEventListener('install', event => {
