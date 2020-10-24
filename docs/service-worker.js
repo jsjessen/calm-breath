e5f569b4d4d3cd735d2e153a7758c6a5ac711b9a
James Jessen <JSJessen@gmail.com>
2020-10-23 23:40:49 -0700

    Changed tips wording

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -8,7 +8,7 @@ const filesToCache = [
   'pages/404.html'
 ];
 
-const staticCacheName = 'cache-v1.14';
+const staticCacheName = 'cache-v1.15';
 console.log(staticCacheName);
 
 self.addEventListener('install', event => {
