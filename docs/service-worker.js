af34252b81ec7127dc880f8a6cbfc1c642a85df0
James Jessen <JSJessen@gmail.com>
2020-10-23 22:41:23 -0700

    Made panic message more concise

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -8,7 +8,7 @@ const filesToCache = [
   'pages/404.html'
 ];
 
-const staticCacheName = 'cache-v1.12';
+const staticCacheName = 'cache-v1.13';
 console.log(staticCacheName);
 
 self.addEventListener('install', event => {
