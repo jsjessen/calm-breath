f1818298b44d8116973d271ad620874690b34b97
James Jessen <JSJessen@gmail.com>
2020-11-21 02:38:53 -0800

    Fixed typo

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -8,7 +8,7 @@ const filesToCache = [
   'pages/404.html'
 ];
 
-const staticCacheName = 'cache-v1.5.1';
+const staticCacheName = 'cache-v1.5.2';
 console.log(staticCacheName);
 
 self.addEventListener('install', event => {
