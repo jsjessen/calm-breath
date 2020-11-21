e3e586d6b7a6d4787ae62191ea3a406c931ffbf4
James Jessen <JSJessen@gmail.com>
2020-11-21 02:25:50 -0800

    Minimized js and css files

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -8,7 +8,7 @@ const filesToCache = [
   'pages/404.html'
 ];
 
-const staticCacheName = 'cache-v1.4.1';
+const staticCacheName = 'cache-v1.5.0';
 console.log(staticCacheName);
 
 self.addEventListener('install', event => {
